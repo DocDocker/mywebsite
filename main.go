65469c008ed2e134fd9d3839a6d7aca2866ba29a
shannon <lucassha@oregonstate.edu>
2019-12-24 09:03:06 -0800

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Static("/static", "static")
+	e.File("/", "./static/index.html")
+	e.File("/posts", "./static/index.html")
+	e.File("/contact", "./static/contact.html")
+	// e.GET("/", helloWorld)
+	// e.GET("/test/:id", getUser)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World")
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	if id == "" {
+		return c.String(http.StatusBadRequest, "ID not valid")
+	}
+
+	return c.String(http.StatusOK, id)
+}

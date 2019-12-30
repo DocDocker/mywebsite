addee83d854bed8aea73c82dc7d5c589d38544d5
Shannon Lucas <slucas@newrelic.com>
2019-12-29 18:55:27 -0800

    add remote s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 provider "aws" {
   region  = "${var.aws_region}"
-  profile = "testing/shannon"
+  profile = "shannon"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "shannon-website"
+    key     = "shannon-website/terraform/terraform.tfstate"
+    region  = "us-west-2"
+    profile = "shannon"
+  }
 }

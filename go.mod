c4d7de228ac3f8c0df3f69877d9bcc5469b8fe55
shannon <lucassha@oregonstate.edu>
2020-10-04 13:51:27 -0700

    allllll the cleanup

---
+++ go.mod
@@ -0,0 +1,12 @@
+module lucassha/mywebsite
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/newrelic/go-agent/v3 v3.9.0
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	golang.org/x/tools v0.0.0-20190524140312-2c0ae7006135 // indirect
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
+)
--- main.go
+++ main.go
@@ -1,47 +1,14 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"github.com/newrelic/go-agent/v3/integrations/nrecho-v3"
-	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func main() {
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName("shannon echo website"),
-		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
-		newrelic.ConfigDebugLogger(os.Stdout),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	e := echo.New()
-
 	e.Use(middleware.Logger())
-	e.Use(nrecho.Middleware(app))
-
 	e.Static("/static", "static")
 	e.File("/", "./static/index.html")
-	e.File("/posts", "./static/index.html")
-	e.File("/contact", "./static/contact.html")
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func helloWorld(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World")
-}
-
-func getUser(c echo.Context) error {
-	id := c.Param("id")
-	if id == "" {
-		return c.String(http.StatusBadRequest, "ID not valid")
-	}
-
-	return c.String(http.StatusOK, id)
 }
